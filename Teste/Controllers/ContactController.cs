9f653806c5f6bfbbf1822f58a351895700dd4980
Administrator <Administrator@WIN-3SMQJ2GO2G5>
2021-08-02 11:43:25 -0700

    Add project files.

---
+++ Teste/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Teste.Models;
+using Teste.Services;
+
+namespace Teste.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
